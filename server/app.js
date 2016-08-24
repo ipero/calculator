0c868e5f0b8a512f0f8e07fa8bedb1e73c5492fa
ipero <ipero@live.com>
2016-08-24 12:17:36 -0500

    Add readme file

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.set("port", (process.env.PORT || 5000));
+app.set("port", (process.env.PORT || 5100));
 
 
 app.use("/", index);
